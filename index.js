f8d59e6b6e7b19ced6a288e4fa0d20781ea0eddf
kuzmich84 <kudim84@gmail.com>
2021-09-08 09:35:20 +0300

    add navigation

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use(express.static('public'));
 
+
+
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'html', 'index.html'));
+    res.render('index');
 
 })
 
-app.get('/about.html', (req, res)=>{
-    res.status(200).sendFile(path.join(__dirname, 'html', 'about.html'));
-})
 
 const PORT = process.env.PORT || 3000;
 
